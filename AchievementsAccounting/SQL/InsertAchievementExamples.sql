05629ce01547f57d912e8611740ac4b44fd10a8c
Margarita Kireeva <margaritakireeva@mail.ru>
2022-06-30 12:11:36 +0400

    added sql achievement examples

---
+++ AchievementsAccounting/SQL/InsertAchievementExamples.sql
@@ -0,0 +1,5 @@
+USE AchievementsAccountingDB
+
+INSERT INTO Achievements
+VALUES ('Начинающий спортсмен', 'Занимайтесь спортом 7 дней подряд'),
+('Англичанин', 'Занимайтесь английским 30 дней подряд')
